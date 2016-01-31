691ac57e4460584b9cdc93acd8adf704cc5543c6
Sinh <sinh9x0@gmail.com>
2016-01-30 22:22:31 -0800

    retrieve airports by start city

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+/* 
+
+    This is index.js
+
+    main: http://collabedit.com/hsyx6, 
+    index.js: http://collabedit.com/vd25f, 
+    intentSchema: http://collabedit.com/hfj4j,
+    SampleUtterances: http://collabedit.com/c3cq7
+
+*/
+
 /**
     Created by Carl Argabright, Aaron Harris, Sinh Le, and Daniel M.
     
@@ -36,9 +47,10 @@
 /**
  * App ID for the skill
  */
-var APP_ID = 'amzn1.echo-sdk-ams.app.34bdad6e-28e4-4c99-81ed-677c4da11366';
+var APP_ID = "amzn1.echo-sdk-ams.app.c4a0b7ae-c4c3-4602-95a2-8613f5b818eb";
 
-var https = require('https');
+// var https = require('https');
+var http = require('http');
 
 /**
  * The AlexaSkill Module that has the AlexaSkill prototype and helper functions
@@ -48,7 +60,7 @@ var AlexaSkill = require('./AlexaSkill');
 /**
  * URL prefix to download history content from Wikipedia
  */
-var urlPrefix = 'http://terminal2.expedia.com/x/';
+var urlPrefix = "http://terminal2.expedia.com/x/";
 
 /**
  * Variable defining number of events to be read at one time
@@ -59,6 +71,8 @@ var paginationSize = 3;
  * Variable defining the length of the delimiter between events
  */
 var delimiterSize = 2;
+
+var customer = {}
 
 /**
  * MyTravelAgentSkill is a child of AlexaSkill.
@@ -97,6 +111,11 @@ MyTravelAgentSkill.prototype.intentHandlers = {
 
     "GetFirstEventIntent": function (intent, session, response) {
         handleFirstEventRequest(intent, session, response);
+    },
+
+
+    "GetLocationsForTravel": function(intent, session, response) {
+        handleGetLocationsRequest(intent, session, response);
     },
 
     "GetNextEventIntent": function (intent, session, response) {
@@ -145,12 +164,11 @@ function getWelcomeResponse(response) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     var cardTitle = "Hot Deals from Expedia";
     
-    //var repromptText = "With History Buff, you can get historical events for any day of the year.  For example, you could say today, or August thirtieth. Now, which day do you want?";
     var repromptText = "Your Travel Agent allows you to book a flight or hotel through Expedia. " +
-            "To start, you can say, book a flight, or, book a hotel.";
+            "To start, you can say, book a flight.";
 
     var speechText = "Your Travel Agent allows you to book a flight or hotel through Expedia. " +
-            "To start, you can say, book a flight, or, book a hotel.";
+            "To start, you can say, book a flight.";
 
     var cardOutput = "My Travel Agent. Would you like to check for airfare, or hotel bookings";
     // If the user either does not reply to the welcome message or says something that is not
@@ -169,15 +187,14 @@ function getWelcomeResponse(response) {
     response.askWithCard(speechOutput, repromptOutput, cardTitle, cardOutput);
 }
 
+
 /**
  * Gets a poster prepares the speech to reply to the user.
  */
 function handleFirstEventRequest(intent, session, response) {
 
-    var typeSlot = intent.slots.type;
-
-    var speechText = "Where would you like to book a " + typeSlot + " to?";
-    var repromptText = "Where would you like to book a " + typeSlot + " to?";
+    var speechText = "Where would you like to travel from and to?";
+    var repromptText = "Where would you like to travel from and to?";
     var cardContent = "";
     var cardTitle = "";
 
@@ -195,148 +212,94 @@ function handleFirstEventRequest(intent, session, response) {
 
 }
 
-function handleEndLocationEventRequest(intent, session, response) {
+function handleGetLocationsRequest(intent, session, response){
+    var startLocation = intent.slots.startLocation;
+    var endLocation = intent.slots.endLocation
+
+    console.log(endLocation);
 
     var sessionAttributes = {};
+
     // Read the first 3 events, then set the count to 3
     sessionAttributes.index = paginationSize;
-    var date = "";
 
-    // If the user provides a date, then use that, otherwise use today
-    // The date is in server time, not in the user's time zone. So "today" for the user may actually be tomorrow
-    if (daySlot && daySlot.value) {
-        date = new Date(daySlot.value);
-    } else {
-        date = new Date();
-    }
+    var cardContent = "";
+    var cardTitle = "Trip from " + startLocation + " to " + endLocation;
+    
+    getAirportsFromExpedia(startLocation, endLocation, function (airports) {
+        var speechText = "";
 
-    var prefixContent = "<p>For " + monthNames[date.getMonth()] + " " + date.getDate() + ", </p>";
-    var cardContent = "For " + monthNames[date.getMonth()] + " " + date.getDate() + ", ";
-    var cardTitle = "Events on " + monthNames[date.getMonth()] + " " + date.getDate();
-
-    getJsonEventsFromWikipedia(monthNames[date.getMonth()], date.getDate(), function (events) {
-        var speechText = "",
-            i;
-        sessionAttributes.text = events;
-        session.attributes = sessionAttributes;
-        if (events.length == 0) {
-            speechText = "There is a problem connecting to Wikipedia at this time. Please try again later.";
+        if (airports.length == 0) {
+            speechText = "There is a problem connecting to Expedia at this time. Please try again later.";
             cardContent = speechText;
-            response.tell(speechText);
+            response.tell("Error with JSON call");
         } else {
-            for (i = 0; i < paginationSize; i++) {
-                cardContent = cardContent + events[i] + " ";
-                speechText = "<p>" + speechText + events[i] + "</p> ";
+
+            var majorAirports = [];
+
+            for (a in airports) {
+                if(!airports[a].isMinorAirport){
+                    majorAirports.push(airports[a].a);
+                    console.log(majorAirports);
+                }
             }
-            speechText = speechText + " <p>Wanna go deeper in history?</p>";
+
+
+            speechText = "Okay. We will start planning your trip to " + endLocation.value + ". When would you like to go?";
             var speechOutput = {
-                speech: "<speak>" + prefixContent + speechText + "</speak>",
+                speech: "<speak>" + speechText + "</speak>",
                 type: AlexaSkill.speechOutputType.SSML
             };
+
             var repromptOutput = {
-                speech: repromptText,
+                speech: speechText,
                 type: AlexaSkill.speechOutputType.PLAIN_TEXT
             };
+
             response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
         }
-    });
-}
 
-/**
- * Gets a poster prepares the speech to reply to the user.
- */
-function handleNextEventRequest(intent, session, response) {
-    var cardTitle = "More events on this day in history",
-        sessionAttributes = session.attributes,
-        result = sessionAttributes.text,
-        speechText = "",
-        cardContent = "",
-        repromptText = "Do you want to know more about what happened on this date?",
-        i;
-    if (!result) {
-        speechText = "With MyTravelAgent, you can get prices for hotels and airfare for any day of the year.  For example, you could say check airfare or get hotel prices. Now, which do you want?";
-        cardContent = speechText;
-    } else if (sessionAttributes.index >= result.length) {
-        speechText = "There are no more events for this date. Try another date by saying <break time = \"0.3s\"/> get events for august thirtieth.";
-        cardContent = "There are no more events for this date. Try another date by saying, get events for august thirtieth.";
-    } else {
-        for (i = 0; i < paginationSize; i++) {
-            if (sessionAttributes.index>= result.length) {
-                break;
-            }
-            speechText = speechText + "<p>" + result[sessionAttributes.index] + "</p> ";
-            cardContent = cardContent + result[sessionAttributes.index] + " ";
-            sessionAttributes.index++;
-        }
-        if (sessionAttributes.index < result.length) {
-            speechText = speechText + " Wanna go deeper in history?";
-            cardContent = cardContent + " Wanna go deeper in history?";
-        }
-    }
-    var speechOutput = {
-        speech: "<speak>" + speechText + "</speak>",
-        type: AlexaSkill.speechOutputType.SSML
-    };
-    var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
-    };
-    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
+    })
+
 }
 
 // We will implement this function to fetch our flight data from Expedia
-function getJsonEventsFromWikipedia(day, date, eventCallback) {
-    var url = urlPrefix + day + '_' + date;
+function getAirportsFromExpedia(startLocation, endLocation, eventCallback) {
 
-    https.get(url, function(res) {
+    var url = urlPrefix + "suggestions/flights?query=seattle&apikey=G8VVfMp8ufGIALoOz236921b4Jt3NmdK";
+
+    http.get(url, function(res) {
         var body = '';
 
-        res.on('data', function (chunk) {
-            body += chunk;
+        res.on('data', function (apiData) {
+            body += apiData;
         });
 
         res.on('end', function () {
-            var stringResult = parseJson(body);
-            eventCallback(stringResult);
+            eventCallback(JSON.parse(body).sr);
         });
+
     }).on('error', function (e) {
+
         console.log("Got error: ", e);
+
     });
+
 }
 
-
-// We need to implement this method to parse back the flight information from Expedia
-function parseJson(inputText) {
-    // sizeOf (/nEvents/n) is 10
-    var text = inputText.substring(inputText.indexOf("\\nEvents\\n")+10, inputText.indexOf("\\n\\n\\nBirths")),
-        retArr = [],
-        retString = "",
-        endIndex,
-        startIndex = 0;
-
-    if (text.length == 0) {
-        return retArr;
-    }
-
-    while(true) {
-        endIndex = text.indexOf("\\n", startIndex+delimiterSize);
-        var eventText = (endIndex == -1 ? text.substring(startIndex) : text.substring(startIndex, endIndex));
-        // replace dashes returned in text from Wikipedia's API
-        eventText = eventText.replace(/\\u2013\s*/g, '');
-        // add comma after year so Alexa pauses before continuing with the sentence
-        eventText = eventText.replace(/(^\d+)/,'$1,');
-        eventText = 'In ' + eventText;
-        startIndex = endIndex+delimiterSize;
-        retArr.push(eventText);
-        if (endIndex == -1) {
-            break;
-        }
-    }
-    if (retString != "") {
-        retArr.push(retString);
-    }
-    retArr.reverse();
-    return retArr;
+function addLegsToOffers(result) {
+    var legObj = {};
+    result.legs.forEach(function(el, index, array) { 
+         legObj[el.legId] = el;
+    });
+    
+    return result.offers.map(function(offer, index, array) {
+        offer.legs = el.legIds.map(function(legId) {
+            return legObj[legId];
+        });
+        return el;
+    });
+    return result;
 }
 
 // Create the handler that responds to the Alexa Request.
@@ -345,4 +308,3 @@ exports.handler = function (event, context) {
     var skill = new MyTravelAgentSkill();
     skill.execute(event, context);
 };
-
